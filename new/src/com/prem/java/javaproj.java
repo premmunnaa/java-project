dc991925e605707ed446fb01a9a908fabeb65a64
premmunnaa <premmano98@gmail.com>
2020-12-18 12:05:11 +0530

    changed first sysout

--- new/src/com/prem/java/javaproj.java
+++ new/src/com/prem/java/javaproj.java
@@ -2,7 +2,7 @@ package com.prem.java;
 
 public class javaproj {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("Vanakkam");
 		System.out.println("2nd commit");
 		System.out.println("3rd commit");
 		System.out.println("4th commit done");
